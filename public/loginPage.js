3eb3a12e9f07f8843787370fe3b2e7cb9e4c88a0
Pavel Shetkov <fertym99@gmail.com>
2021-10-24 17:09:24 +0300

    bjs-diploma

---
+++ public/loginPage.js
@@ -0,0 +1,13 @@
+'user srtict'
+
+const { response } = require("express");
+
+const userForm = new UserForm();
+let data = {};
+userForm.loginFormCallback = data => ApiConnector.login({ login, password }, callback => {
+  if( userForm.loginFormAction() ){
+  return userForm.location.reload
+  }else{
+      return loginErrorMessageBox
+  }
+})
