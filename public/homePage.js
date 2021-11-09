1bda3c98afd83ff0c7fa8731760de2885de8e491
Pavel Shetkov <fertym99@gmail.com>
2021-11-09 15:00:34 +0300

    er

--- public/homePage.js
+++ public/homePage.js
@@ -1,31 +1,23 @@
 'use strict'
-
-const { response } = require("express");
-
 const logoutButton = new LogoutButton();
 logoutButton.action = () =>{
-    ApiConnector.logout((response) =>{
-if(response.success){
-    location.reload();
-}else{
-    console.error(`${response.error}`);
-}
+    ApiConnector.logout((serverResponse) =>{
+location.reload();
+
     })
 };
-let current = ApiConnector.current((response) =>{
-    if(response.success){
-        ProfileWidget.showProfile(response.data);
-    }else{
-        console.error(`Ошибка вывода пользователя`);
-    }
+let current = ApiConnector.current((serverResponse) =>{
+    
+        ProfileWidget.showProfile(serverResponse.data);
+    
 
 });
 const rateBoard = new RatesBoard();
 function getExchangeRate(){
-    ApiConnector.getStocks((response) => {
-        if(response.success){
+    ApiConnector.getStocks((serverResponse) => {
+        if(serverResponse.success){
           rateBoard.clearTable();
-          rateBoard.fillTable(response.data);
+          rateBoard.fillTable(serverResponse.data);
         }
         else{
             console.error('Ошибка вывода курсов валют');
@@ -34,39 +26,73 @@ function getExchangeRate(){
 }
 getExchangeRate();
 setInterval(getExchangeRate(),60000);
+
 const moneyManger = new MoneyManger();
 moneyManger.addMoneyCallback() = (data) =>{
-    ApiConnector.addMoney(data,(response) =>{
-        if(response.success){
-            ProfileWidget.showProfile(response.data);
+    ApiConnector.addMoney(data,(serverResponse) =>{
+        if(serverResponse.success){
+            ProfileWidget.showProfile(serverResponse.data);
             moneyManger.setMessage(true, 'Пополнения балланса успешно');
         }else{
-            moneyManger.setMessage(false, 'Произошла ошибка при пополнение баланка');
+            moneyManger.setMessage(false, `Произошла ошибка при пополнение баланка ${serverResponse.error}`);
          }
 
 });
 }
 moneyManger.conversionMoneyCallback() = (data) =>{
-    ApiConnector.convertMoney(data,(response) => {
-        if(response.success){
-            ProfileWidget.showProfile(response.data);
+    ApiConnector.convertMoney(data,(serverResponse) => {
+        if(serverResponse.success){
+            ProfileWidget.showProfile(serverResponse.data);
             moneyManger.setMessage(true,'Конвертация прошла успешно');
        
         }else{
-            moneyManger.setMessage(false, 'Конверт ация не произошла');
+            moneyManger.setMessage(false, `Конвертация не произошла ${serverResponse.error}`);
         }
     })
 }
 moneyManger.sendMoneyCallback() = (data) =>{
-    ApiConnector.transferMoney(data,(response) =>{
-        if(response.success){
-            ProfileWidget.showProfile(response.data);
+    ApiConnector.transferMoney(data,(serverResponse) =>{
+        if(serverResponse.success){
+            ProfileWidget.showProfile(serverResponse.data);
             moneyManger.setMessage(true,'Трансфер прошел успешно');
         }else{
-            moneyManger.setMessage(false,'Трансфер не прошел');
+            moneyManger.setMessage(false,`Трансфер не прошел ${serverResponse.error}`);
         }
-    })
+    });
 }
+ const favoritesWidget = FavoritesWidget();
+ ApiConnector.getFavorites(data,(serverResponse) =>{
+    if(serverResponse.success){    
+        favoritesWidget.clearTable();
+         favoritesWidget.fillTable(serverResponse.data);
+         favoritesWidget.updateUsersList(serverResponse.data);
+    }
+     
+ });
+ favoritesWidget.addUserCallback() = (data) =>{
+     ApiConnector.addUserToFavorites(data ,(serverResponse) =>{
+       if(serverResponse.success){
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(serverResponse.data);
+        favoritesWidget.updateUsersList(serverResponse.data);
+       }else{
+           favoritesWidget.setMessage(`Не получилось добавить в Избранные ${serverResponse.error}`);
+       }
+     });
+ }
+ favoritesWidget.removeUserCallback() = (data) =>{
+     ApiConnector.removeUserFromFavorites(data,(serverResponse) =>{
+         if(serverResponse.success){
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(serverResponse.data);
+            favoritesWidget.updateUsersList(serverResponse.data);
+           }else{
+               favoritesWidget.setMessage(`Не получилось удалить из Избраного ${serverResponse.error}`);
+         }
+     })
+ }
+
+
 
        
  
